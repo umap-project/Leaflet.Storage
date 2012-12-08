f69fd7aeaae6534b48099b416474ffd168c855be
Yohan Boniface <yb@enix.org>
2012-12-08 13:49:06 +0100

    Extract JS lib from the django project yohanboniface/django-chickpea#19

--- chickpea/__init__.py
+++
@@ -1,7 +0,0 @@
-"Create collaborative maps on top of Geodjango and Leaflet."
-VERSION = (0, 0, 4)
-
-__author__ = 'Yohan Boniface'
-__contact__ = "yb@enix.fr"
-__homepage__ = "https://github.com/yohanboniface/django-chickpea"
-__version__ = ".".join(map(str, VERSION))
--- chickpea/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from chickpea.base_models import (Category, Map, TileLayer, Pictogram, Licence,
-                                  MapToTileLayer, get_model)
-
-# Marker is the only model configurable for now
-Marker = get_model("Marker")
-Polyline = get_model("Polyline")
-Polygon = get_model("Polygon")
