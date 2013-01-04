e07eb2d94928f08511c1c677561e117a60c466d2
Yohan Boniface <yb@enix.org>
2013-01-04 13:36:49 +0100

    Add German translation, thanks to Mathis

---
+++ src/locale/de.js
@@ -0,0 +1,27 @@
+de = {
+    // controls.js
+    "Go to home page": "Zur Startseite zurückkehren",
+    "Caption and credits": "Légence et crédits",
+    "Add a category": "Katergorie hinzufügen",
+    "Edit": "Bearbeiten",
+    "Embed this map": "Karte einbinden",
+    "Edit map infos": "Karteninformation bearbeiten",
+    "Update permissions and editors": "Berechtigungen und Bearbeiter ändern",
+    "Change tilelayers": "Hintergrundkarte ändern",
+    "Save this center and zoom": "Aktuelle Position und Zoomstufe speichern",
+    "Enable/disable editing": "Aktiviere/ Deaktiviere die Bearbeitung",
+    "Upload data": "Daten hochladen",
+    // features.js
+    "Feature updated with success!": "Objekt erfolgreich geändert",
+    // layer.js
+    "Unkown geometry.type: {type}": "Ungültiger Geometrie Typ: {type}",
+    "Category successfuly edited": "Kategorie erfolgreich geändert.",
+    // storage.js
+    "Successfully updated tilelayers": "Kartenhintergrund erfolgreich geändert",
+    // Xhr.js
+    "Problem in the response format": "Ungültiges Format der Serverantwort",
+    "Action not allowed :(": "Diese Aktion ist nicht erlaubt :(",
+    "Problem in the response": "Fehlerhafte Serverantwort",
+    "Double-click to edit": "Doppelklicken zum Editieren"
+};
+L.S.registerLocale("de", de);
