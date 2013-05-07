1708d59c5bcbc318e3a11f4fd8de1e2c5accc8d3
Tim Schwarte <tim@timschwarte.nl>
2013-05-07 21:13:58 +0300

    Create nl.js

---
+++ src/locale/nl.js
@@ -0,0 +1,34 @@
+var de = {
+    "Go to home page": "Ga naar homepagina",
+    "Caption and credits": "Credits",
+    "Add a category": "Voeg categorie toe",
+    "Edit": "Bewerken",
+    "Embed this map": "Sluit deze kaart in",
+    "Edit map infos": "Bewerk kaart informatie",
+    "Update permissions and editors": "Bijwerken permissies en bewerkers",
+    "Change tilelayers": "Verander tilelayer",
+    "Save this center and zoom": "Bewaar deze positie en zoomniveau",
+    "Enable/disable editing": "Toestaan/blokkeren bewerken",
+    "Upload data": "Data uploaden",
+    "Feature updated with success!": "Eigenschap succesvol bijgewerkt",
+    "Unkown geometry.type: {type}": "Onbekend geometrisch type: {type}",
+    "Category successfuly edited": "Categorie succesvol bewerkt.",
+    "Successfully updated tilelayers": "Tilelayer succesvol bewerkt",
+    "Problem in the response format": "Probleem met het server antwoord formaat",
+    "Action not allowed :(": "Handeling niet toegestaan :(",
+    "Problem in the response": "Probleem met het server antwoord",
+    "Double-click to edit": "Dubbelklikken om te bewerken",
+    "Center map on your location": "Centreer kaart op je locatie",
+    "Change shape": "Verander vorm",
+    "Change symbol": "Verander symbool",
+    "Choose a shape": "Kies een vorm",
+    "Edit map settings": "Bewerk kaart instellingen",
+    "Jump to location": "Spring naar locatie",
+    "Remove icon shape": "Verwijder icoon vorm",
+    "Remove icon symbol": "Verwijder icoon symbool",
+    "Sorry, no location found for {location}": "Helaas, geen locatie gevonden voor {location}"
+};
+
+
+L.S.registerLocale("nl", nl);
+
