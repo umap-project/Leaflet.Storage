2b8cbc5707404bf88e7a5e3aa2417f784140e83a
Yohan Boniface <yb@enix.org>
2013-05-15 15:21:47 +0200

    Add empty Finnish locale file

---
+++ src/locale/fi.js
@@ -0,0 +1,32 @@
+var fi = {
+    "Action not allowed :(": "",
+    "Add a category": "",
+    "Caption and credits": "",
+    "Category successfuly edited": "",
+    "Center map on your location": "",
+    "Change shape": "",
+    "Change symbol": "",
+    "Change tilelayers": "",
+    "Choose a shape": "",
+    "Double-click to edit": "",
+    "Edit": "",
+    "Edit map infos": "",
+    "Edit map settings": "",
+    "Embed this map": "",
+    "Enable/disable editing": "",
+    "Feature updated with success!": "",
+    "Go to home page": "",
+    "Jump to location": "",
+    "Problem in the response": "",
+    "Problem in the response format": "",
+    "Remove icon shape": "",
+    "Remove icon symbol": "",
+    "Save this center and zoom": "",
+    "Sorry, no location found for {location}": "",
+    "Successfully updated tilelayers": "",
+    "Unkown geometry.type: {type}": "",
+    "Update permissions and editors": "",
+    "Upload data": ""
+};
+
+L.registerLocale("fi", fi);
