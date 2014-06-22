851e7780bc1c3b9982df91f52e91e282c5310043
Yohan Boniface <yb@enix.org>
2014-06-22 20:12:22 +0200

    put table editor fullwidth only if more than 2 properties

--- src/js/leaflet.storage.tableeditor.js
+++ src/js/leaflet.storage.tableeditor.js
@@ -101,7 +101,8 @@ L.S.TableEditor = L.Class.extend({
             this.edit();
         };
         L.DomEvent.on(addButton, 'click', addProperty, this);
-        L.S.fire('ui:start', {data: {html: this.table}, cssClass: 'storage-table-editor', actions: [addButton]});
+        var className = (this.properties.length > 2) ? 'storage-table-editor fullwidth' : 'storage-table-editor';
+        L.S.fire('ui:start', {data: {html: this.table}, cssClass: className, actions: [addButton]});
         this.datalayer.map.fire('dataload', {id: id});
     }
 
