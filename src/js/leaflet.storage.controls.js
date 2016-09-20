a44ace470648f74d4fa889347d34b6dba5e5befb
Yohan Boniface <yb@enix.org>
2016-09-21 01:32:24 +0200

    Add layer delete link in the layer toolbox

--- src/js/leaflet.storage.controls.js
+++ src/js/leaflet.storage.controls.js
@@ -579,15 +579,23 @@ L.Storage.DataLayer.include({
         var toggle = L.DomUtil.create('i', 'layer-toggle', container),
             zoomTo = L.DomUtil.create('i', 'layer-zoom_to', container),
             edit = L.DomUtil.create('i', 'layer-edit show-on-edit', container),
-            table = L.DomUtil.create('i', 'layer-table-edit show-on-edit', container);
+            table = L.DomUtil.create('i', 'layer-table-edit show-on-edit', container),
+            remove = L.DomUtil.create('i', 'layer-delete show-on-edit', container);
         zoomTo.title = L._('Zoom to layer extent');
         toggle.title = L._('Show/hide layer');
         edit.title = L._('Edit');
         table.title = L._('Edit properties in a table');
+        remove.title = L._('Delete layer');
         L.DomEvent.on(toggle, 'click', this.toggle, this);
         L.DomEvent.on(zoomTo, 'click', this.zoomTo, this);
         L.DomEvent.on(edit, 'click', this.edit, this);
         L.DomEvent.on(table, 'click', this.tableEdit, this);
+        L.DomEvent.on(remove, 'click', function () {
+                    if (!this.isVisible()) return;
+                    if (!confirm(L._('Are you sure you want to delete this layer?'))) return;
+                    this._delete();
+                    this.map.ui.closePanel();
+                }, this);
         L.DomUtil.addClass(container, this.getHidableClass());
         L.DomUtil.classIf(container, 'off', !this.isVisible());
         container.dataset.id = L.stamp(this);
